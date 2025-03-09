71b842adf12af6854f1e35073d8b1f0a673c9960
Ahmed Abdul Rehman <ahmedabdulrehman2005@gmail.com>
2025-03-10 02:17:21 +0500

    Created Java Series

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//import java.lang.*;
+public class Main {
+    public static void main(String[] args) {
+        int myNum;
+        myNum = 89;
+        System.out.println(myNum);
+//        System.out.println(Main.main());
+    }
+}
