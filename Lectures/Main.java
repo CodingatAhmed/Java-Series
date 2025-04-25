00eecfb8295cca9f97490f71ed200931b1a0e39a
Ahmed Abdul Rehman <ahmedabdulrehman2005@gmail.com>
2025-04-25 13:08:18 +0500

    Added new files

--- Lectures/Main.java
+++ Lectures/Main.java
@@ -1,36 +1,34 @@
+import java.io.*;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 //import java.lang.*;
 public class Main {
+
+//    public static void takeInput(){
+//        Scanner intInput = new Scanner(System.in);
+//        System.out.println("Enter your number");
+//        int newInput = intInput.nextInt();
+//        System.out.println(newInput);
+//    }
     public static void main(String[] args) {
-
-
-        // 0 1 1 2 3 5 8
-
-        int x;
-        int y;
-        int result;
-        int temp;
-        int count;
-        int num;
-
-        Scanner userinput = new Scanner(System.in);
-
-        System.out.println("Enter number till you want to generate fibonacci");
-        num = userinput.nextInt();
-
-        x = 0;
-        y = 1;
-        count = 0;
-        result = 0;
-        while (count < num){
-            result = x + y;
-            temp = y;
-            y = result ;
-            x = temp;
-            count = count + 1;
+        try {
+            FileWriter fileOpen = new FileWriter("enterData.txt");
+            fileOpen.write("Hello");
+            System.out.println("Hello");
+//            Scanner takeInput = new Scanner(System.in);
+//            System.out.println("Enter data you want to write in file");
+//            String enterData = takeInput.nextLine();
+//            while (enterData != ""){
+//                fileOpen.write(enterData);
+//                enterData = takeInput.nextLine();
+            }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//            throw new RuntimeException(e);
+//            System.out.println(e);
         }
-
-        System.out.println("The "+num+"th"+" of fibonacci is: "+result);
     }
 }
